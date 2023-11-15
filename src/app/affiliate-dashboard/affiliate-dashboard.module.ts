0b65b9661191d971da44c58e44e0a324fa64f331
sourjyadeep <sourjyadeep@influxiqtech.com>
2023-11-15 19:07:54 +0530

    update

--- src/app/affiliate-dashboard/affiliate-dashboard.module.ts
+++ src/app/affiliate-dashboard/affiliate-dashboard.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AffiliateDasboardRoutingModule } from './affiliate-dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from '../shared/shared.module';
-import { LastLoginInfoComponent } from '../Common-components/last-login-info/last-login-info.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent,
-    LastLoginInfoComponent
+    DashboardComponent
   ],
   imports: [
     CommonModule,
