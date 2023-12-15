587fc5cd20e48c2cd0265dded21e24f2539bbb14
sanketinfluxiq <sanket@influxiqtech.com>
2023-12-15 11:09:47 +0530

    pie chart implemented

---
+++ src/app/affiliate-dashboard/pie-chart/pie-chart.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css'],
+})
+export class PieChartComponent implements OnChanges {
+
+  constructor() { }
+
+  public chartData: any;
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  @Input() chart: any;
+
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  createChart() {
+    this.chartData = new Chart('myChart', {
+      type: 'pie', //this denotes tha type of chart
+      data: {
+        // values on X-Axis
+        labels: ['Red', 'Pink', 'Green', 'Yellow', 'Orange', 'Blue'],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [300, 240, 100, 432, 253, 34],
+            backgroundColor: [
+              'red',
+              'pink',
+              'green',
+              'yellow',
+              'orange',
+              'blue',
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+}
